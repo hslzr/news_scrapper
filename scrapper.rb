f4e87033d47002bc6bd0673e9eec6ef5c30fe47d
Hector Salazar <h.salazar@hlcn.mx>
2017-10-26 18:52:52 -0500

    Adds link to (sample) output

--- scrapper.rb
+++ scrapper.rb
@@ -3,13 +3,13 @@ require 'active_support/all'
 Bundler.require(:default)
 Dir['./newspapers/*.rb'].each {|file| require file}
 
-articles = Newspaper::Financiero.articles
+articles = Newspaper::Reforma.articles
 
 print ""
 
 articles.each do |article|
   puts "-> #{article[:title]}"
-  puts "-- #{article[:pub_date]}"
+  puts "#{article[:pub_date]} -- #{article[:link]}"
   puts "#{article[:description]}"
   puts " "
 end
